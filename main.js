c4b496660283c1935b52302a9feac37e88cb0bf1
Abdulrahman-boop <abdulrahmanmansour600@gmail.com>
2021-06-05 12:08:55 +0200

    first comment

---
+++ main.js
@@ -0,0 +1,84 @@
+var container=document.getElementById("container");
+console.log(container);
+for(var i =1;i<9;i++){
+    let box1=document.createElement("div");
+    if(i % 2 ==0){
+        box1.style.backgroundColor="black";
+    }
+    else{
+        box1.style.backgroundColor="white";
+    }
+    container.appendChild(box1);
+}
+for(var i =1;i<9;i++){
+    let box2=document.createElement("div");
+    if(i % 2 ==0){
+        box2.style.backgroundColor="white";
+    }
+    else{
+        box2.style.backgroundColor="black";
+    }
+    container.appendChild(box2);
+}
+for(var i =1;i<9;i++){
+    let box3=document.createElement("div");
+    if(i % 2 ==0){
+        box3.style.backgroundColor="black";
+    }
+    else{
+        box3.style.backgroundColor="white";
+    }
+    container.appendChild(box3);
+}
+for(var i =1;i<9;i++){
+    let box4=document.createElement("div");
+    if(i % 2 ==0){
+        box4.style.backgroundColor="white";
+    }
+    else{
+        box4.style.backgroundColor="black";
+    }
+    container.appendChild(box4);
+}
+for(var i =1;i<9;i++){
+    let box5=document.createElement("div");
+    if(i % 2 ==0){
+        box5.style.backgroundColor="black";
+    }
+    else{
+        box5.style.backgroundColor="white";
+    }
+    container.appendChild(box5);
+}
+for(var i =1;i<9;i++){
+    let box6=document.createElement("div");
+    if(i % 2 ==0){
+        box6.style.backgroundColor="white";
+    }
+    else{
+        box6.style.backgroundColor="black";
+    }
+    container.appendChild(box6);
+}
+for(var i =1;i<9;i++){
+    let box7=document.createElement("div");
+    if(i % 2 ==0){
+        box7.style.backgroundColor="black";
+    }
+    else{
+        box7.style.backgroundColor="white";
+    }
+    container.appendChild(box7);
+}
+for(var i =1;i<9;i++){
+    let box8=document.createElement("div");
+    if(i % 2 ==0){
+        box8.style.backgroundColor="white";
+    }
+    else{
+        box8.style.backgroundColor="black";
+    }
+    container.appendChild(box8);
+}
+
+
